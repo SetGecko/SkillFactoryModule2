1a534e53e27f5b989f20346b63fcbe8e75d411b5
Ипполитов Алексей <setgecko@gmail.com>
2023-02-03 22:48:33 +0300

    быстрая инициализация переменной

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Алексей";
+            string MyName = "Алексей";
 
             Console.WriteLine(MyName);
 
