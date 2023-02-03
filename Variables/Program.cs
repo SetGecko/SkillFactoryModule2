aa7d09cba1bc5135062b037f8eb594f40a64e5c0
Ипполитов Алексей <setgecko@gmail.com>
2023-02-03 22:46:27 +0300

    Добавьте файлы проекта.

---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Алексей";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
