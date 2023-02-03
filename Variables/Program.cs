eeaa9c6ebf94c09932cbd43ce608f5c463c0f342
Ипполитов Алексей <setgecko@gmail.com>
2023-02-03 22:53:13 +0300

    Практика с литералами

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -8,6 +8,10 @@
 
             Console.WriteLine(MyName);
 
+            Console.WriteLine("Привет, мир!");
+            Console.WriteLine("Мне 35 лет.");
+            Console.WriteLine("Мой имя Алексей");
+
 
             Console.ReadKey();
         }
