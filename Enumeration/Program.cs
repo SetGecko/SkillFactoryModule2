41e593c493d17bbbbec16086d8b33504ce9ee6a0
Ипполитов Алексей <setgecko@gmail.com>
2023-02-05 06:13:40 +0300

    Практика по теме Перечисления

---
+++ Enumeration/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek MyFavoriteDay = DayOfWeek.Sunday;
+
+            Console.WriteLine(MyFavoriteDay);
+        }
+    }
+    
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    enum Semaphore : int
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
+    }
+}
