226ff6d6cfa4d84351f614cf0576c918ab767cf9
Ипполитов Алексей <setgecko@gmail.com>
2023-02-05 07:51:51 +0300

    Выполнил задания 3.6.7 и 3.6.8

---
+++ Task_3.6.7/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_3._6._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 10 % 3;
+            Console.WriteLine(result);
+        }
+    }
+}
