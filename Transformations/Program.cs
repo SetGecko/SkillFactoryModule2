011167c58fe3facc9cc5624972793414ec7b0d0a
Ипполитов Алексей <setgecko@gmail.com>
2023-02-05 08:05:26 +0300

    Практика преобразований 3.7.3

---
+++ Transformations/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Transformations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.ReadKey();
+        }
+    }
+}
