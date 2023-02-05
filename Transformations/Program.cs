5149d5d907178647dad4d6f3fa0d7831d71d625b
Ипполитов Алексей <setgecko@gmail.com>
2023-02-05 08:10:36 +0300

    Задание 3.7.4

--- Transformations/Program.cs
+++ Transformations/Program.cs
@@ -7,9 +7,24 @@
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.Write("Enter your age: ");
-            byte age = (byte)int.Parse(Console.ReadLine());
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
             Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-            Console.ReadKey();
+
+            Console.WriteLine("What is your favorite day of week?");
+            DaysOfWeek day = (DaysOfWeek) int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day is {0}", day);
+
+
         }
+    }
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
