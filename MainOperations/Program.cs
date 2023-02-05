9f6e362988810fe2bce03cc067e38077360de2e1
Ипполитов Алексей <setgecko@gmail.com>
2023-02-05 07:32:11 +0300

    Практика по сосновным операциям

---
+++ MainOperations/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MainOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 5f / 2f;
+            Console.WriteLine("5 / 2 = {0}", result);
+        }
+    }
+}
