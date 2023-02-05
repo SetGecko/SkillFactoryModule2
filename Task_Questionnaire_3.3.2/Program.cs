13188ebe439a19ab912dfda8ae16058e9db21894
Ипполитов Алексей <setgecko@gmail.com>
2023-02-05 05:33:02 +0300

    Сделал задание по видам переменным 3.3.2

--- Task_Questionnaire_3.3.2/Program.cs
+++ Task_Questionnaire_3.3.2/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            string myName = "Алексей";
+            byte myAge = 35;
+            bool haveIPet = true;
+            double myShoesSize = 41.5;
+
+            Console.WriteLine("Меня зовут " + myName);
+            Console.WriteLine("Мой возраст " + myAge);
+            Console.WriteLine("У меня есть питомец? " + haveIPet);
+            Console.WriteLine("Мой размер ноги " + myShoesSize);
         }
     }
 }
